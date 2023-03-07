b4841e8f589b2f35e36f1caec40b206fb99b82d7
Bhumika-Chauhan09 <103961358+Bhumika-Chauhan09@users.noreply.github.com>
2023-03-07 20:02:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const base = `${__dirname}/public`;
+const cors=require("cors");
 app.use(express.static('public'));
+app.use(cors());
+
 
 app.get('/', function (req, res) {
   res.sendFile(`${base}/device-list.html`);
@@ -23,4 +26,8 @@ app.get('/iot-applications', (req, res) => {
 });
 app.get('*', (req, res) => {
   res.sendFile(`${base}/404.html`);
+});
+app.get('*', (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials","true");
+  res.send("API is running");
 });
